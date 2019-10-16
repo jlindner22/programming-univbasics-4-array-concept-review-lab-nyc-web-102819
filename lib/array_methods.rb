27a897e9b29bc749c00b0a990e14c2940e97ecc2
Jen Lindner <jenlindner22@gmail.com>
2019-10-16 15:43:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,17 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+count = 0 
+while count < array.length do
+  puts array.rindex(value_to_find)
+  value_to_find += 1
+end
 end
 
 def find_max_value(array)
-  # Add your solution here
+array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+array.min
 end
+
+
